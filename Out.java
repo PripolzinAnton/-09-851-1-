41f49f46c5b6845a371a6ad00441d6c61a9d95e8
AntonPripolzin <PripolzinAnton@yandex.ru>
2019-02-24 17:45:32 +0300

    Third test

---
+++ Out.java
@@ -0,0 +1,7 @@
+import java.io.PrintStream;
+public class OUT {
+public static PrintStream out = System.out;
+public static void main (String [], args) {
+out.println ("This is not my first program");
+}
+}
